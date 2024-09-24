7fd0f4af0495a8711c36eef371e795ffe4f61062
FelTrxbajos <fe.moralesf@duocuc.cl>
2024-09-23 22:13:47 -0300

    Final del trabajo, revisar correcciones

--- src/app/pages/administrar/administrar.page.ts
+++ src/app/pages/administrar/administrar.page.ts
@@ -1,25 +1,86 @@
 import { Component, OnInit } from '@angular/core';
-import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { AbstractControl, FormControl, FormGroup, Validators, ValidatorFn } from '@angular/forms';
+import { AlertController } from '@ionic/angular';
+import { UsuarioService } from 'src/app/services/usuario.service';
+
 
 @Component({
   selector: 'app-administrar',
   templateUrl: './administrar.page.html',
   styleUrls: ['./administrar.page.scss'],
 })
-export class AdministrarPage implements OnInit {
-  persona = new FormGroup({
-    rut: new FormControl('',[Validators.minLength(9),Validators.maxLength(10),Validators.required]),
-    nombre: new FormControl('',[Validators.required,Validators.pattern("[a-z,A-Z]{3,9}")]),
-    fecha_nacimiento: new FormControl('',[Validators.required]),
-    //VALIDAR!! FECHA NACIMIENTO
-    genero: new FormControl('',[Validators.required]),
-    tiene_equipo: new FormControl('no',Validators.required),
-    nombre_equipo: new FormControl('',[])
+export class AdministrarPage  implements OnInit {
+    persona = new FormGroup({
+    nombre: new FormControl('', [Validators.required, Validators.pattern(/^[A-Za-zÁÉÍÓÚáéíóúñÑ\s]+(\s[A-Za-zÁÉÍÓÚáéíóúñÑ]+)*$/)]),
+    rut: new FormControl('', [Validators.required, Validators.pattern(/^\d{6,8}-[kK0-9]$/)]),
+    genero: new FormControl('', [Validators.required]),
+    fecha_nacimiento: new FormControl('', [Validators.required, this.anosvalidar(18, 100)]),
+    correo: new FormControl('', [Validators.required, Validators.pattern("[a-zA-Z0-9._%+-]+@duocuc.cl")]),
+    password: new FormControl('', [Validators.required, Validators.minLength(4)]),
+    tiene_auto: new FormControl('no', [Validators.required]),
+    patente: new FormControl('', [Validators.pattern(/^[A-Z]{2}[0-9]{4}$|^[A-Z]{4}[0-9]{2}$/)]),
+    asientos_disp: new FormControl('', [Validators.min(1)]),
+    tipouser: new FormControl('usuario', [Validators.required]),
   });
-    
-  constructor() { }
+
+  usuarios: any[] = [];
+
+  constructor(private alertController: AlertController, private usuarioService: UsuarioService) { }
+
 
   ngOnInit() {
+    this.usuarios = this.usuarioService.getUsuarios();
   }
 
+  async registrar() {
+    if (this.usuarioService.createUsuario(this.persona.value)) {
+      await this.presentAlert('Perfecto', 'Registrado correctamente');
+      this.persona.reset();
+      this.usuarios = this.usuarioService.getUsuarios();
+    } else {
+      await this.presentAlert('Error', 'El usuario no se pudo registrar');
+    }
+  }
+
+  buscar(usuario: any) {
+    this.persona.patchValue(usuario);
+  }
+
+  eliminar(rut: string) {
+    if (this.usuarioService.deleteUsuario(rut)) {
+      this.usuarios = this.usuarioService.getUsuarios();
+    }
+  }
+
+  modificar() {
+    var rut_modificar = this.persona.controls.rut.value || "";
+    if (this.usuarioService.updateUsuario(rut_modificar, this.persona.value)) {
+      this.presentAlert('Perfecto!', 'Modificado correctamente');
+    } else {
+      this.presentAlert('Error!', 'No se pudo modificar');
+    }
+  }
+  anosvalidar(minAge: number, maxAge: number): ValidatorFn {
+    return (control: AbstractControl): { [key: string]: any } | null => {
+      const birthDate = new Date(control.value);
+      const today = new Date();
+      let age = today.getFullYear() - birthDate.getFullYear();
+      const monthDiff = today.getMonth() - birthDate.getMonth();
+
+      if (monthDiff < 0 || (monthDiff === 0 && today.getDate() < birthDate.getDate())) {
+        age--;
+      }
+
+      return (age >= minAge && age <= maxAge) ? null : { 'invalidAge': true };
+    };
+  }
+
+  async presentAlert(header: string, message: string) {
+    const alert = await this.alertController.create({
+      header: header,
+      message: message,
+      buttons: ['Entendido'],
+    });
+    await alert.present();
+  }
 }
--- src/app/pages/registrarse/registrarse.page.ts
+++ src/app/pages/registrarse/registrarse.page.ts
@@ -15,13 +15,13 @@ export class RegistrarsePage implements OnInit {
   usuario: any[] = [];
 
   constructor(private alertController: AlertController, private usuarioService: UsuarioService) {
-    this.user = new FormGroup({
-      email: new FormControl('', [Validators.required, Validators.pattern("[a-zA-Z0-9._%+-]+@duocuc.cl")]),
-      name: new FormControl('', [Validators.required, Validators.pattern("[a-zA-Z]{3,15}")]),
+      this.user = new FormGroup({
+      name: new FormControl('', [Validators.required, Validators.pattern(/^[A-Za-zÁÉÍÓÚáéíóúñÑ\s]+(\s[A-Za-zÁÉÍÓÚáéíóúñÑ]+)*$/)]),
       rut: new FormControl('', [Validators.required, Validators.pattern(/^\d{6,8}-[kK0-9]$/)]),
+      email: new FormControl('', [Validators.required, Validators.pattern("[a-zA-Z0-9._%+-]+@duocuc.cl")]),
       birthdate: new FormControl('', [Validators.required, this.anosvalidar(18, 100)]),
       tiene_auto: new FormControl('no', [Validators.required]),
-      marca_auto: new FormControl('', [this.validarMarcaAuto.bind(this)]),
+      marca_auto: new FormControl('', []),
       asientos_disp: new FormControl('', []),
       patente: new FormControl('', [Validators.pattern(/^[A-Z]{2}[0-9]{4}$|^[A-Z]{4}[0-9]{2}$/)]),
       gender: new FormControl('', [Validators.required]),
@@ -30,21 +30,7 @@ export class RegistrarsePage implements OnInit {
     }, { validators: this.passwordMatchValidator }); 
   }
   
-  
 
-  marcaAuto: string[] = [
-    'abarth', 'acura', 'alfa romeo', 'audi', 'bmw', 'bentley', 'buick', 'cadillac',
-    'chevrolet', 'citroën', 'dodge', 'fiat', 'ford', 'genesis', 'honda', 'hyundai',
-    'infiniti', 'jaguar', 'jeep', 'kia', 'lamborghini', 'land rover', 'lexus',
-    'lincoln', 'maserati', 'mazda', 'mclaren', 'mercedes benz', 'mini', 'mitsubishi',
-    'nissan', 'pagani', 'peugeot', 'porsche', 'ram', 'renault', 'rolls royce',
-    'saab', 'seat', 'skoda', 'smart', 'subaru', 'suzuki', 'tesla', 'toyota',
-    'volkswagen', 'volvo', 'byd', 'jac', 'changan', 'great wall', 'geely',
-    'haval', 'mg', 'brilliance', 'foton', 'lynk & co', 'dongfeng', 'xpeng',
-    'nio', 'ora', 'rivian', 'polestar', 'karma', 'landwind', 'zotye',
-    'wuling', 'baojun', 'gac', 'hummer'
-  ];
-  
 
   ngOnInit() {
     this.usuario = this.usuarioService.getUsuarios();
@@ -56,16 +42,7 @@ export class RegistrarsePage implements OnInit {
     return password && confirmPassword && password !== confirmPassword ? { passwordsDoNotMatch: true } : null;
   }
 
-  
 
-
-  validarMarcaAuto(control: AbstractControl) {
-    const marca = control.value ? control.value.toLowerCase() : '';
-    if (marca && !this.marcaAuto.includes(marca)) {
-      return { marcaNoExiste: true };
-    }
-    return null;
-  }
 
 
   anosvalidar(minAge: number, maxAge: number): ValidatorFn {
@@ -82,14 +59,6 @@ export class RegistrarsePage implements OnInit {
       return (age >= minAge && age <= maxAge) ? null : { 'invalidAge': true };
     };
   }
-
-
-
-
-
-
-
-
   async presentAlert(header: string, message: string) {
     const alert = await this.alertController.create({
       header: header,
@@ -98,10 +67,6 @@ export class RegistrarsePage implements OnInit {
     });
     await alert.present();
   }
-
-
-
-  
   async submit() {
     if (this.user.valid) {
       await this.presentAlert('Perfecto!', 'Registrado correctamente');
